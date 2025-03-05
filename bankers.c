b310d80a91e54694f20cbd4f0bbb8b5b0d1a3d40
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-03-06 03:05:51 +0530

    done bankers

---
+++ bankers.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+
+void accept(int m,int n,int alloc[][n],int max[][n],int need[][n],int available[][n]){
+  printf("Enter the total no: of resource: ");
+  for(int i=0;i<n;i++){
+    scanf("%d",&available[0][i]);
+  }
+
+  for(int i=0;i<m;i++){
+    printf("\nEnter the details of P%d\n",i+1);
+    printf("Allocation: ");
+    for(int j=0;j<n;j++){
+      scanf("%d",&alloc[i][j]);
+      available[0][j] -= alloc[i][j];
+    }
+
+    printf("Max: ");
+    for(int j=0;j<n;j++){
+      scanf("%d",&max[i][j]);
+      need[i][j] = max[i][j] - alloc[i][j];
+    }
+  }
+}
+
+int isSafe(int m,int n,int alloc[][n],int max[][n],int need[][n],int available[][n],int safeseq[]){
+  int finish[10] = {0};
+  int work[n];
+  for(int i=0;i<n;i++){
+    work[i] = available[0][i];
+  }
+
+  int k = 1,count;
+  do{
+    count = 0;
+    for(int i=0;i<m;i++){
+      if(finish[i]){
+        continue;
+      }
+
+      int canAllocate = 1;
+      for(int j=0;j<n;j++){
+        if(need[i][j] > work[j]){
+          canAllocate = 0;
+          break;
+        }
+      }
+      
+      if(canAllocate){
+        for(int j=0;j<n;j++){
+          work[j] += alloc[i][j];
+          available[k][j] = work[j];
+        }
+        safeseq[k-1] = i+1;
+        finish[i] = 1;
+        k++;
+        count++;
+      }
+    }
+  }while(count > 0);
+
+  for(int i=0;i<m;i++){
+    if(!finish[i]){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void display(int m,int n,int alloc[][n],int max[][n],int need[][n],int available[][n]){
+  printf("\nProcess\t\tAllocation\tMax\t\tNeed\t\tAvailable");
+  for(int i=0;i<m;i++){
+    printf("\n  P%d\t\t",i+1);
+    for(int j=0;j<n;j++){
+      printf("%d ",alloc[i][j]);
+    }
+    printf("\t");
+    for(int j=0;j<n;j++){
+      printf("%d ",max[i][j]);
+    }
+    printf("\t");
+    for(int j=0;j<n;j++){
+      printf("%d ",need[i][j]);
+    }
+    printf("\t");
+    for(int j=0;j<n;j++){
+      printf("%d ",available[i][j]);
+    }
+  }
+}
+
+void main(){
+  int m,n;
+  printf("Enter the no: of processes: ");
+  scanf("%d",&m);
+  printf("Enter the no: of resources: ");
+  scanf("%d",&n);
+  
+  int alloc[m][n],max[m][n],need[m][n],available[m+1][n],safeseq[m];
+  accept(m,n,alloc,max,need,available);
+  
+  if(isSafe(m,n,alloc,max,need,available,safeseq)){
+    display(m,n,alloc,max,need,available);
+
+    printf("\n\nSystem is in safe state");
+    printf("\nSafe sequence: ");
+    for(int i=0;i<m;i++){
+      printf("P%d ",safeseq[i]);
+    }
+  }else{
+    printf("\nSystem is in unsafe state");
+  }
+}
